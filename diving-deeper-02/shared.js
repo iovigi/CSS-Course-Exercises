48790a1c19cfd8f58349f572b4120e0b9c0132a5
iovigi <il.nedelchev@gmail.com>
2019-02-14 18:36:30 +0200

    commit close btn

--- diving-deeper-02/shared.js
+++ diving-deeper-02/shared.js
@@ -1,10 +1,20 @@
 var backdrop = document.querySelector(".backdrop");
-var modal = document.querySelector('.modal');
-var selectPlanButtons = document.querySelectorAll('.plan .button');
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan .button");
+var modalCloseBtn = document.querySelector(".modal__action--negative");
 
 selectPlanButtons.forEach(x => {
-     x.addEventListener('click',function(){
-        backdrop.style.display = "block";
-        modal.style.display = 'block';
-     });
+  x.addEventListener("click", function() {
+    backdrop.style.display = "block";
+    modal.style.display = "block";
+  });
 });
+
+backdrop.addEventListener("click", closeModal);
+
+modalCloseBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
